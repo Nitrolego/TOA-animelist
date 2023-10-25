80a9a7f270c34f8484ec52ba96eb450eea4c5fdd
Boon Chuan Tan <boonchuan.tan@beyondsoft.com>
2023-10-25 14:17:37 +0800

    remote push

--- youranimelist.js
+++ youranimelist.js
@@ -1,20 +1,53 @@
-const api_url = "https://api.jikan.moe/v4/anime/1/full"; 
-async function getAnime() { 
+var api_url = "https://api.jikan.moe/v4/anime/{id}/full"; 
+
+
+async function getAnime(id) { 
       
     // Making an API call (request) 
     // and getting the response back 
-    const response = await fetch(api_url); 
+    const response = await fetch(api_url.replace("{id}", id)); 
 
+    if (!response.ok) {
+        console.log("not ok " + id)
+        return;
+    } else {  
     // Parsing it to JSON format 
-    const data = await response.json(); 
+    const data = await response.json();
 
     // Retrieving data from JSON
     const anime = data.data;
-    let {title_english} = anime
+    let {title, url} = anime;
+    let images = anime.images.jpg.large_image_url;
 
     // Accessing the div container and modify/add 
-    // elements to the containers 
-    document.getElementById("animename").innerHTML = title_english;
+    // elements to the containers
+
+    var container = document.createElement("div")
+
+    var link = document.createElement("a")
+    link.href = url
+    var img = document.createElement("img");
+    img.className = "anime-img"
+    img.src = images
+    link.appendChild(img)
+    container.appendChild(link)
+    
+    var text = document.createElement("div");
+    text.className = "anime-name"
+    text.innerHTML = title;
+    container.appendChild(text)
+
+    document.body.appendChild(container)
+
+    //success, i can make the div dynamically.
+    //now i need to make sure that only success is counted when looping through.
+    //it's also really slow.
+    }
 }
 
-getAnime();
+for (let i = 1; i < 15; i++) {
+    getAnime(i);
+}
+
+// try catch block might be more suitable for this.
+
