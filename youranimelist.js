a6c604cb2feea982f5d236840015f5dc0e6643db
Boon Chuan Tan <boonchuan.tan@beyondsoft.com>
2023-10-26 15:06:09 +0800

    converted to numbers of success format

--- youranimelist.js
+++ youranimelist.js
@@ -1,19 +1,33 @@
 var api_url = "https://api.jikan.moe/v4/anime/{id}/full"; 
+let counter = 0
+let id = 1;
 
+async function getAnime(numberOfRequest) { 
+    if(counter >= numberOfRequest) {
+        return;
+    }
 
-async function getAnime(id) { 
-      
+    let response;
+    try {
     // Making an API call (request) 
     // and getting the response back 
-    const response = await fetch(api_url.replace("{id}", id)); 
-
+    response = await fetch(api_url.replace("{id}", id)); 
+    }
+    catch(err) {
+        console.log("There was an error with this request")
+        return;
+    }
+    
     // check if response is OK
     // logging if not ok and return to skip the rest of the block
     if (!response.ok) {
         console.log("The anime id " + id + " has failed to repond with OK")
+        //increment the id number and keep searching until number of request is reached.
+        id++
+        getAnime(numberOfRequest)
         return;
     } else {  
-
+    
     // Parsing it to JSON format 
     const data = await response.json();
 
@@ -48,12 +62,43 @@ async function getAnime(id) {
 
     // add container into HTML body of index.html
     document.body.appendChild(container)
+    
+    //counters to progress the function until all anime is gotten
+    counter++
+    id++
+    getAnime(numberOfRequest)
     }
 }
 
 // repeat api calls for first 100 ids in Jikan
-for (let i = 1; i < 100; i++) {
-    getAnime(i);
-}
+// for (let i = 1; i < 100; i++) {
+//     getAnime(i, 20)
+// }
+
+getAnime(20)
+
+// let i = 1;
+// while (success < 20) {
+//     getAnime(i);
+//     i++;
+// }
+
+// while (success < 20) {
+//     getAnime(i).then((response) => {
+//         if (response.ok) {
+//             success += 1;
+//             i++;
+//             console.log(success);
+//             console.log(i)
+//         }
+//         i++;
+//     });
+// }
+
+// do {
+//     getAnime(i);
+//     i++
+// }
+// while (success < 20);
 
 
